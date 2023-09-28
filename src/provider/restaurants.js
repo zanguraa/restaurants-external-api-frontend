5d16747aecff87b45ce16886aad0b1500d036045
Valeri Zanguri <zanguraa@gmail.com>
2023-09-28 12:02:59 +0400

    feat: upload project

---
+++ src/provider/restaurants.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+export const RestaurantsInitialState = {
+  restaurants: [],
+  starredRestaurants: [],
+};
+
+export const RestaurantsReducer = (state = RestaurantsInitialState, action) => {
+  switch (action.type) {
+    case "LOADED_RESTAURANTS": {
+      return { ...state, restaurants: action.payload };
+    }
+    case "LOADED_STARRED_RESTAURANTS": {
+      return { ...state, starredRestaurants: action.payload };
+    }
+    case "ADD_NEW_RESTAURANT": {
+      return { ...state, restaurants: [...state.restaurants, action.payload] };
+    }
+    case "DELETE_RESTAURANT": {
+      return {
+        ...state,
+        restaurants: state.restaurants.filter(
+          (restaurant) => restaurant.id !== action.payload
+        ),
+        starredRestaurants: state.starredRestaurants.filter( starred => starred.restaurantId !== action.payload)
+      };
+    }
+    case "UPDATE_RESTAURANT_NAME": {
+      const nextRestaurantsState = [...state.restaurants];
+      const restaurantToUpdate = nextRestaurantsState.find(
+        (restaurant) => restaurant.id === action.payload.id
+      );
+      const nextStarredState = [...state.starredRestaurants]
+      const starredToUpdate = nextStarredState.find(starred => starred.restaurantId === action.payload.id)
+
+      if(starredToUpdate) {
+        starredToUpdate.name = action.payload.newName
+      }
+
+      restaurantToUpdate.name = action.payload.newName;
+      return {
+        ...state,
+        restaurants: nextRestaurantsState,
+        starredRestaurants: nextStarredState
+      };
+    }
+    case "STAR_RESTAURANT": {
+      return {
+        ...state,
+        starredRestaurants: [...state.starredRestaurants, action.payload],
+      };
+    }
+    case "UNSTAR_RESTAURANT": {
+      return {
+        ...state,
+        starredRestaurants: state.starredRestaurants.filter(
+          (restaurant) => restaurant.id !== action.payload
+        ),
+      };
+    }
+    case "UPDATE_STARRED_RESTAURANT_COMMENT": {
+      const nextStarredRestaurantsState = [...state.starredRestaurants];
+      const restaurantToUpdate = nextStarredRestaurantsState.find(
+        (restaurant) => restaurant.id === action.payload.id
+      );
+
+      restaurantToUpdate.comment = action.payload.newComment;
+
+      return { ...state, starredRestaurants: nextStarredRestaurantsState };
+    }
+    default:
+      return state;
+  }
+};
+
+const RestaurantsContext = React.createContext({
+  state: RestaurantsInitialState,
+  dispatch: (action) => {},
+});
+
+export default RestaurantsContext;
